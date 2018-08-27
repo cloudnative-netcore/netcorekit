3a7cafa73736c34704e1d4358049bf7935e073db
Thang Chung <thangchung@ymail.com>
2018-08-27 17:00:27 +0700

    fixed sub-path config

--- src/NetCoreKit.Infrastructure.AspNetCore/Extensions/HostUriExtensions.cs
+++ src/NetCoreKit.Infrastructure.AspNetCore/Extensions/HostUriExtensions.cs
@@ -13,37 +13,40 @@ namespace NetCoreKit.Infrastructure.AspNetCore.Extensions
 
     public static string GetInternalHostUri(this IConfiguration config, string groupName)
     {
-      var group =
-        config
+      var group = config
           .GetSection("Hosts")
           ?.GetSection("Internals")
           ?.GetSection(groupName);
 
       var serviceHost = $"{Environment.GetEnvironmentVariable(group.GetValue<string>("Host"))}";
       var servicePort = $"{Environment.GetEnvironmentVariable(group.GetValue<string>("Port"))}";
+      var basePath = $"{group.GetValue("BasePath", string.Empty)}";
 
-      return $"http://{serviceHost}:{servicePort}";
+      return $"http://{serviceHost}:{servicePort}{basePath}";
     }
 
     public static string GetExternalHostUri(this IConfiguration config, string groupName)
     {
-      var group =
-        config
+      return config
           .GetSection("Hosts")
           ?.GetSection("Externals")
-          ?.GetSection(groupName);
-
-      return group.GetValue<string>("Uri");
+          ?.GetSection(groupName)
+          ?.GetValue<string>("Uri");
     }
 
     public static string GetBasePath(this IConfiguration config)
     {
-      return config.GetSection("Hosts")?.GetValue<string>("BasePath");
+      return config
+        .GetSection("Hosts")
+        ?.GetValue<string>("BasePath");
     }
 
     public static string GetExternalCurrentHostUri(this IConfiguration config)
     {
-      return config.GetSection("Hosts")?.GetSection("Externals")?.GetValue<string>("CurrentUri");
+      return config
+        .GetSection("Hosts")
+        ?.GetSection("Externals")
+        ?.GetValue<string>("CurrentUri");
     }
   }
 }
