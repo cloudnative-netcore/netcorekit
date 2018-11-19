7d73bd2ecca259fcd00538ee070044c6b2afbd1a
Thang Chung <thangchung@ymail.com>
2018-11-19 14:33:35 +0700

    change the name of configuration

--- samples/BiMonetaryApi/Controllers/TickerController.cs
+++ samples/BiMonetaryApi/Controllers/TickerController.cs
@@ -13,13 +13,13 @@ namespace NetCoreKit.Samples.BiMonetaryApi.Controllers
     public int Rank { get; set; }
     public double PriceUsd { get; private set; }
     public double PriceBtc { get; private set; }
-    public double Volumn24hUsd { get; private set; }
+    public double Volumn24HUsd { get; private set; }
     public double MarketCapUsd { get; private set; }
     public double AvailableSupply { get; private set; }
     public double TotalSupply { get; private set; }
-    public string PercentChange1h { get; private set; }
-    public string PercentChange24h { get; private set; }
-    public string PercentChange7d { get; private set; }
+    public string PercentChange1H { get; private set; }
+    public string PercentChange24H { get; private set; }
+    public string PercentChange7D { get; private set; }
     public DateTime LastSyncWithService { get; private set; }
     public string Link { get; private set; }
   }
