3ea1e40c40e44871bbc421eebb1c4275b29e9a58
Thang Chung <thangchung@ymail.com>
2018-11-25 23:17:05 +0700

    #23 fixed build failed

--- samples/TodoApi/Startup.cs
+++ samples/TodoApi/Startup.cs
@@ -2,9 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using NetCoreKit.Infrastructure.AspNetCore.Miniservice;
 using NetCoreKit.Infrastructure.Bus;
-using NetCoreKit.Infrastructure.Bus.Kafka;
 using NetCoreKit.Infrastructure.Bus.Redis;
-using NetCoreKit.Infrastructure.EfCore;
 using NetCoreKit.Infrastructure.EfCore.MySql;
 using NetCoreKit.Samples.TodoAPI.Domain;
 using NetCoreKit.Samples.TodoAPI.Infrastructure.Db;
@@ -16,7 +14,7 @@ namespace NetCoreKit.Samples.TodoAPI
   {
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMiniService<TodoListDbContext>(
+      services.AddEfCoreMiniService<TodoListDbContext>(
         svc =>
         {
           //svc.AddEfSqlLiteDb();
