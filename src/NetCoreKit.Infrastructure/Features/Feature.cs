9fe522233bbc6ac628a7d11f9667b8b213120907
Thang Chung <thangchung@ymail.com>
2018-12-18 16:21:49 +0700

    simplify feature toogle

--- src/NetCoreKit.Infrastructure/Features/Feature.cs
+++ src/NetCoreKit.Infrastructure/Features/Feature.cs
@@ -13,7 +13,7 @@ namespace NetCoreKit.Infrastructure.Features
 
     public bool IsEnabled(string feature)
     {
-      var featureValue = _config.GetValue($"Features:{feature}:Enabled", false);
+      var featureValue = _config.GetSection($"Features:{feature}").Exists() || _config.GetValue($"Features:{feature}", false);
       return featureValue;
     }
   }
